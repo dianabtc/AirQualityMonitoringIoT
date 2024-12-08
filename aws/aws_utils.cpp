d1dba793efc11beb939729b552d40e939a857098
dianabtc <dianabutacu2002@yahoo.com>
2024-12-08 14:23:42 +0200

    Fixed time sync

--- aws/aws_utils.cpp
+++ aws/aws_utils.cpp
@@ -80,6 +80,10 @@ void publishSensorData(const char* sensor, float value) {
     }
 
     time_t now = time(nullptr);
+    if (now < 100000) {
+    Serial.println("Unsynchronized time, not publishing data.");
+    return;
+    }
     struct tm* timeInfo = localtime(&now);
 
     char timeBuffer[30];
