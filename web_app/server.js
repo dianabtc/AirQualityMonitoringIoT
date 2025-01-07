e5dc6cf90125b53f3cec80e302faebe8abcedb04
dianabtc <dianabutacu2002@yahoo.com>
2025-01-07 21:31:32 +0200

    Added notifications

--- web_app/server.js
+++ web_app/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const cors = require('cors');
+const sns = new AWS.SNS({ region: 'eu-central-1' });
 app.use(cors());
 app.use(express.json());
 
@@ -92,3 +93,40 @@ app.post('/send-command', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Running on http://localhost:${PORT}`);
 });
+
+setInterval(async () => {
+    const params = {
+        TableName: 'SensorData',
+        KeyConditionExpression: 'sensorType = :sensorType',
+        ExpressionAttributeValues: {
+            ':sensorType': 'gas'
+        },
+        Limit: 10,
+        ScanIndexForward: false
+    };
+
+    try {
+        const data = await dynamoDB.query(params).promise();
+        const latestData = data.Items.reverse()[9];
+        console.log('Latest data: ', latestData);
+
+        if (latestData && latestData.value > 1500) {
+            const notificationParams = {
+                Message: `Gas concentration is too high! Value: ${latestData.value}`,
+                TopicArn: 'arn:aws:sns:eu-central-1:463470938276:AirQualityAlerts',
+            };
+
+            try {
+                const snsResponse = await sns.publish(notificationParams).promise();
+                console.log(`Notification sent. Message ID: ${snsResponse.MessageId}`);
+            } catch (snsError) {
+                console.error(`Failed to send notification: ${snsError.message}`);
+            }
+        }
+    } catch (error) {
+        console.error('Error in periodic alert check:', error);
+    }
+}, 10000);
+
+
+
